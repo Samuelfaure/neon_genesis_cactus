90470d5ff645439a46c20eab5b96a29beced324a
Samuelfaure <samuel.faure.dev@gmail.com>
2019-10-19 00:59:08 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import p5 from "p5";
+import "p5/lib/addons/p5.sound";
 
-Vue.config.productionTip = false
+Object.defineProperty(Vue.prototype, "$p5", { value: p5 });
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
