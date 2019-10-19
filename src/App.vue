41b7ee9a35435cb4870898870f25298dee3fb81a
Samuelfaure <samuel.faure.dev@gmail.com>
2019-10-19 15:06:41 +0200

    getting the sound on screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <sound-controls />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import SoundControls from "./components/SoundControls.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    SoundControls
   }
 };
 </script>
---
+++ src/components/SoundControls.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <vue-p5 v-if="record" @setup="setup" @draw="draw"></vue-p5>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+    <p>Level : {{ level }}</p>
+  </div>
+</template>
+
+<script>
+import VueP5 from "vue-p5";
+
+export default {
+  name: "SoundControls",
+  components: {
+    "vue-p5": VueP5
+  },
+  data() {
+    return {
+      record: false,
+      level: 0,
+      mic: null
+    };
+  },
+  methods: {
+    setup() {
+      this.mic = new this.$p5.AudioIn();
+      this.mic.start();
+    },
+    start() {
+      this.record = true;
+    },
+    stop() {
+      this.record = false;
+    },
+    draw() {
+      this.level = this.mic.getLevel();
+    }
+  }
+};
+</script>
+
+<style lang="scss"></style>
