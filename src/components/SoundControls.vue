864392e563daa5d0efd7fe85c350494c9c135e8f
Samuelfaure <samuel.faure.dev@gmail.com>
2019-10-30 01:56:10 +0100

    working on instance mode

--- src/components/SoundControls.vue
+++ src/components/SoundControls.vue
@@ -1,43 +1,61 @@
 <template>
   <div>
-    <vue-p5 v-if="record" @setup="setup" @draw="draw"></vue-p5>
-    <button @click="start">Start</button>
-    <button @click="stop">Stop</button>
+    <button @click="keypress">Start</button>
     <p>Level : {{ level }}</p>
+    <div id="canvas" ref="canvas5"></div>
   </div>
 </template>
 
 <script>
-import VueP5 from "vue-p5";
+import p5 from "p5";
+import "p5/lib/addons/p5.sound";
 
 export default {
   name: "SoundControls",
-  components: {
-    "vue-p5": VueP5
-  },
   data() {
     return {
-      record: false,
       level: 0,
-      mic: null
+      mic: null,
+      myp5: null
     };
   },
   methods: {
-    setup() {
-      this.mic = new this.$p5.AudioIn();
-      this.mic.start();
-    },
-    start() {
-      this.record = true;
-    },
-    stop() {
-      this.record = false;
-    },
-    draw() {
-      this.level = this.mic.getLevel();
+    keypress() {
+      const script = function(s) {
+        let speed = 2;
+        let posX = 0;
+
+        s.setup = () => {
+          s.createCanvas(1000, 1000);
+          s.ellipse(s.width / 2, s.height / 2, 500, 500);
+        };
+        s.draw = () => {
+          s.background(0);
+          const degree = s.frameCount * 3;
+          const y = s.sin(s.radians(degree)) * 50;
+
+          s.push();
+          s.translate(0, s.height / 2);
+          s.ellipse(posX, y, 50, 50);
+          s.pop();
+          posX += speed;
+
+          if (posX > s.width || posX < 0) {
+            speed *= -1;
+          }
+        };
+      };
+
+      this.myp5 = new p5(script, "canvas");
     }
   }
 };
 </script>
 
-<style lang="scss"></style>
+<!-- <style> -->
+<!-- .canvas { -->
+<!--   background-color: green; -->
+<!--   width: 500px; -->
+<!--   height: 500px; -->
+<!-- } -->
+<!-- </style> -->
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
-import p5 from "p5";
-import "p5/lib/addons/p5.sound";
 
-Object.defineProperty(Vue.prototype, "$p5", { value: p5 });
+// Object.defineProperty(Vue.prototype, "$p5", { value: p5 });
 
 Vue.config.productionTip = false;
 
