53f8fe0d0a73c18d24486c3e3cf0d2dcc4f9afbc
Samuelfaure <samuel.faure.dev@gmail.com>
2019-10-30 04:21:39 +0100

    We have a beat

--- src/components/SoundControls.vue
+++ src/components/SoundControls.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <button @click="keypress">Start</button>
+    <button @click="start">Start</button>
+    <button @click="resume">Resume</button>
+    <button @click="log">log</button>
     <p>Level : {{ level }}</p>
-    <div id="canvas" ref="canvas5"></div>
+    <p>Radius : {{ radius }}</p>
+    <div id="canvas"></div>
   </div>
 </template>
 
@@ -14,48 +17,65 @@ export default {
   name: "SoundControls",
   data() {
     return {
+      fft: null,
       level: 0,
+      radius: 0,
       mic: null,
       myp5: null
     };
   },
   methods: {
-    keypress() {
-      const script = function(s) {
-        let speed = 2;
-        let posX = 0;
+    resume() {
+      this.context.resume();
+      console.log(this.context);
+    },
+    log() {
+      console.log(this.level);
+    },
+    setupCanvas(s) {
+      this.mic = new p5.AudioIn();
+      this.mic.start();
+      this.context = s.getAudioContext();
+      this.fft = new p5.FFT();
+      this.fft.setInput(this.mic);
 
+      s.createCanvas(600, 600);
+      // Anti-aliasing
+      s.smooth();
+
+      s.fill(0, 155, 0);
+      s.stroke(0, 0, 0);
+      s.strokeWeight(2);
+
+      s.frameRate(30);
+    },
+    drawCanvas(s) {
+      s.background(255);
+
+      this.fft.analyze();
+      this.level = this.fft.getEnergy("bass");
+
+      s.ellipseMode(s.CENTER);
+      this.radius = this.calculRadius();
+      s.ellipse(300, 300, this.radius, this.radius);
+    },
+    calculRadius() {
+      let radius = this.level * 6 - 800;
+      if (radius < 0) radius = 0;
+      return radius;
+    },
+    start() {
+      const vm = this;
+      const script = function(s) {
         s.setup = () => {
-          s.createCanvas(1000, 1000);
-          s.ellipse(s.width / 2, s.height / 2, 500, 500);
+          vm.setupCanvas(s);
         };
         s.draw = () => {
-          s.background(0);
-          const degree = s.frameCount * 3;
-          const y = s.sin(s.radians(degree)) * 50;
-
-          s.push();
-          s.translate(0, s.height / 2);
-          s.ellipse(posX, y, 50, 50);
-          s.pop();
-          posX += speed;
-
-          if (posX > s.width || posX < 0) {
-            speed *= -1;
-          }
+          vm.drawCanvas(s);
         };
       };
-
-      this.myp5 = new p5(script, "canvas");
+      if (!this.myp5) this.myp5 = new p5(script, "canvas");
     }
   }
 };
 </script>
-
-<!-- <style> -->
-<!-- .canvas { -->
-<!--   background-color: green; -->
-<!--   width: 500px; -->
-<!--   height: 500px; -->
-<!-- } -->
-<!-- </style> -->
