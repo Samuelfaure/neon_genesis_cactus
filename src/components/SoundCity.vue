f106e7e64e1aa93dfe085f9b505c5982c867f8d5
Samuelfaure <samuel.faure.dev@gmail.com>
2020-01-21 18:09:54 +0100

    Framework available for sound experiment

--- src/components/SoundCity.vue
+++ src/components/SoundCity.vue
@@ -2,12 +2,13 @@
   <div>
     <h3 class="text-center">Experiment Lambda</h3>
     <v-btn depressed small color="pink accent-1" @click="start">Start</v-btn>
-    <v-btn depressed small color="grey accent-1" @click="stop">Stop</v-btn>
     <div id="canvas"></div>
   </div>
 </template>
 
 <script>
+// Not implemented yet
+
 import p5 from "p5";
 import "p5/lib/addons/p5.sound";
 
@@ -16,7 +17,7 @@ export default {
   data() {
     return {
       fft: null,
-      script: null
+      myp5: null
     };
   },
   methods: {
@@ -28,16 +29,27 @@ export default {
       this.fft.setInput(mic);
 
       s.createCanvas(500, 500, s.WEBGL);
+
+      // s.fill(0, 155, 0);
+      // s.stroke(0, 0, 0);
+      // s.strokeWeight(2);
     },
     drawCanvas(s) {
       s.background(255);
+      // s.rotateX(s.frameCount * 0.01);
+      // s.rotateY(s.frameCount * 0.01);
+      let i;
+      for (i = 0; i < 30; i++) {
+        s.translate(-150, 0, (100 + 1) * i);
+        s.box(100, 300, 100);
+        s.translate(300, 0, 0);
+        s.box(100, 300, 100);
+        s.translate(-150, 0, 0);
+      }
     },
     start() {
       const vm = this;
-
       const script = function(s) {
-        vm.script = s;
-
         s.setup = () => {
           vm.setupCanvas(s);
         };
@@ -45,10 +57,7 @@ export default {
           vm.drawCanvas(s);
         };
       };
-      new p5(script, "canvas");
-    },
-    stop() {
-      this.script.remove();
+      if (!this.myp5) this.myp5 = new p5(script, "canvas");
     }
   }
 };
